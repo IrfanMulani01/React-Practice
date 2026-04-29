8af7ce1833414399f45f48222d91b9f6f63bc3bd
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-04-29 12:06:06 +0530

    import component counterApp

--- First-Project/src/main.jsx
+++ First-Project/src/main.jsx
@@ -2,10 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Disp from './App'
+import CounterApp from './counter'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     
     <Disp/>
+    <CounterApp />
   </StrictMode>,
 )
