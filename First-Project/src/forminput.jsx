2a7ad397a6e23515168738b0871c4bd652855320
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-04-29 12:46:54 +0530

    add name and email for accept with the help of onchange set name to name and email to email and export the component

--- First-Project/src/forminput.jsx
+++ First-Project/src/forminput.jsx
@@ -1,9 +1,30 @@
-import { useState } from "react";
+import { use, useState } from "react";
 
 const Forminput = () => {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
     return (
         <>
-            
+        Name: 
+            <input 
+                type="text" 
+                value={name} 
+                onChange={(e) => setName(e.target.value)} />
+        Email: 
+            <input 
+                type="email" 
+                value={email} 
+                onChange={(e) => setEmail(e.target.value)} />
+
+            <button onClick={disp}>Submit</button>
         </>
     )
+
+    function disp() {
+        console.log("Name:", name);
+        console.log("Email:", email);
+        alert(`Name: ${name} \nEmail: ${email}`);
+    }
 }
+
+export default Forminput;
