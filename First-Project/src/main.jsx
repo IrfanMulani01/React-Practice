b48a86176a9e1c2bde395f269e34c2913d9614c7
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-04-29 12:47:53 +0530

    import the Forminput component

--- First-Project/src/main.jsx
+++ First-Project/src/main.jsx
@@ -4,11 +4,14 @@ import './index.css'
 import Disp from './App'
 import CounterApp from './counter'
 import Toggle from './toggle'
+import Forminput from './forminput'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Toggle/>
     <Disp/>
     {/* <CounterApp /> */}
+
+    <Forminput />
   </StrictMode>,
 )
