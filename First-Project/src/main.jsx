74172d9caa2b5282f341f004ece31cbcd550258f
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-04-29 12:25:35 +0530

    import the component of toggle

--- First-Project/src/main.jsx
+++ First-Project/src/main.jsx
@@ -3,11 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Disp from './App'
 import CounterApp from './counter'
+import Toggle from './toggle'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    
+    <Toggle/>
     <Disp/>
-    <CounterApp />
+    {/* <CounterApp /> */}
   </StrictMode>,
 )
