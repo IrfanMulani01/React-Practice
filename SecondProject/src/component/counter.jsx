fd10237c8a354b5b8f7db17d02bb6ff73a4ac514
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-05-02 13:48:57 +0530

    in return add two element of html h1 and two buttons(increment, decrement)

--- SecondProject/src/component/counter.jsx
+++ SecondProject/src/component/counter.jsx
@@ -11,12 +11,12 @@ const Counter = () => {
         setcount(count-1)
     }
 
-    // return(
-    //     <>
-    //         <h1>count {count}</h1>
-    //         <button onClick={increment}>Increment</button>
-    //         <button onClick={decrement}>Decrement</button>
-    //     </>
-    // )
+    return(
+        <>
+            <h1>count {count}</h1>
+            <button onClick={increment}>Increment</button>
+            <button onClick={decrement}>Decrement</button>
+        </>
+    )
 
 }
