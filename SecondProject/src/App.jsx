16259c3e409c2769b423b0d857bca7f0c7ce5efb
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-05-02 11:23:07 +0530

    import Card component and render it in different component

--- SecondProject/src/App.jsx
+++ SecondProject/src/App.jsx
@@ -1,17 +1,19 @@
+import Card from "./component/card"
+
+
 const Num = () => {
-  const num = 10
   return(
   <>
-    <h2>Number is {num}</h2>
+    <Card/>
   </>)
 }
 
 
-export const Name = () => {
-  const name = "irfan"
-  return(
-    <h2>Name is {name}</h2>
-  )
-}
+// export const Name = () => {
+//   const name = "irfan"
+//   return(
+//     <h2>Name is {name}</h2>
+//   )
+// }
 
 export default Num
