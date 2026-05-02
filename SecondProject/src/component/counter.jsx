693fb86f5504762aeb6ced13e83bf4d88d2a475f
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-05-02 13:47:25 +0530

    create two function increment and decrement to count + or -

--- SecondProject/src/component/counter.jsx
+++ SecondProject/src/component/counter.jsx
@@ -3,4 +3,20 @@ import { useState } from "react"
 const Counter = () => {
     const [count, setcount] = useState(0)
     
+    function increment(){
+        setcount(count + 1)
+    }
+
+    function decrement() {
+        setcount(count-1)
+    }
+
+    // return(
+    //     <>
+    //         <h1>count {count}</h1>
+    //         <button onClick={increment}>Increment</button>
+    //         <button onClick={decrement}>Decrement</button>
+    //     </>
+    // )
+
 }
